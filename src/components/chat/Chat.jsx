2de9a633d4e4e57905210f70778d7b07771ad5cf
kayro-is <luffykosuki@gmail.com>
2024-04-13 01:06:23 +0200

    aujourdui jai travailler sur le fichier chatList et chatjsx

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,7 +2,26 @@ import './chat.css'
 
 const Chat = () => {
     return (
-        <div className="chat">Chat</div>
+        <div className="chat">
+            <div className='top'>
+            <div className='user'>
+                <img src="./avatar.png" alt="" />
+                <div className='texts'>
+                    <span> Ishaaq Mehadi 🥰 </span>
+                    <p>{`Mon Fils, l'amour de ma vie 💘`}</p>
+                </div>
+            </div>
+            <div className='icons'>
+                <img src="./phone.png" alt="" />
+                <img src="./video.png" alt="" />
+                <img src="./info.png" alt="" />
+
+            </div>
+            </div>
+            <div className='center'></div>
+            <div className='bottom'></div>
+            
+        </div>
     )
 }
 
