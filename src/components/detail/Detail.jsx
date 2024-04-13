1f3360d26d59f8cdf07428d060c513e8d645d2c2
kayro-is <luffykosuki@gmail.com>
2024-04-14 01:06:04 +0200

    Conception des composants de chat detail

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,107 @@ import './detail.css'
 
 const Detail = () => {
     return (
-        <div className="detail">Detail</div>
+        <div className="detail">
+            <div className='user' >
+                <img src="./avatar.png" alt="" />
+                <h2>Lyes Dre</h2>
+                <p>Lorem, ipsum dolor sit amet .</p>
+            </div>
+            <div className='info'>
+                <div className='option'>
+                    <div className='title'>
+                        <span>Paramétre du Chat</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className='option'>
+                    <div className='title'>
+                        <span>Confidentialité & d’aide</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className='option'>
+                    <div className='title'>
+                        <span>photos partagées</span>
+                        <img src="./arrowDown.png" alt="" />
+                        </div>
+                        <div className='photos'>
+                        <div className='photoItem'>
+                            <div className='photoDetail'>
+                            <img src="violet.jpg" alt="" />
+                   <span>photo_2024_2.png</span>
+                   </div>
+                   <img src="./download.png" alt="" className='icon' />
+                        </div>
+                        <div className='photoItem'>
+                            <div className='photoDetail'>
+                            <img src="violet.jpg" alt="" />
+                   <span>photo_2024_2.png</span>
+                   </div>
+                   <img src="./download.png" alt=""className='icon' />
+                        </div>
+                        <div className='photoItem'>
+                            <div className='photoDetail'>
+                            <img src="violet.jpg" alt="" />
+                   <span>photo_2024_2.png</span>
+                   </div>
+                   <img src="./download.png" alt="" className='icon'/>
+                        </div>
+                        <div className='photoItem'>
+                            <div className='photoDetail'>
+                            <img src="violet.jpg" alt="" />
+                   <span>photo_2024_2.png</span>
+                   </div>
+                   <img src="./download.png" alt=""className='icon' />
+                        </div>
+                        <div className='photoItem'>
+                            <div className='photoDetail'>
+                            <img src="violet.jpg" alt="" />
+                   <span>photo_2024_2.png</span>
+                   </div>
+                   <img src="./download.png" alt=""className='icon' />
+                        </div>
+                        <div className='photoItem'>
+                            <div className='photoDetail'>
+                            <img src="violet.jpg" alt="" />
+                   <span>photo_2024_2.png</span>
+                   </div>
+                   <img src="./download.png" alt=""className='icon' />
+                        </div>
+                        <div className='photoItem'>
+                            <div className='photoDetail'>
+                            <img src="violet.jpg" alt="" />
+                   <span>photo_2024_2.png</span>
+                   </div>
+                   <img src="./download.png" alt=""className='icon' />
+                        </div>
+                        <div className='photoItem'>
+                            <div className='photoDetail'>
+                            <img src="violet.jpg" alt="" />
+                   <span>photo_2024_2.png</span>
+                   </div>
+                   <img src="./download.png" alt=""className='icon' />
+                        </div>
+                        <div className='photoItem'>
+                            <div className='photoDetail'>
+                            <img src="violet.jpg" alt="" />
+                   <span>photo_2024_2.png</span>
+                   </div>
+                   <img src="./download.png" alt="" className='icon'/>
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div className='option'>
+                    <div className='title'>
+                        <span>Fichiers partagés</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <button>Bloquer l’utilisateur</button>
+            </div>
+        </div>
     )
 }
 
